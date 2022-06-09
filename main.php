45bc1a3987e2f0d36886e765011ad16f5a21bfa7
Jacob Kipchirchir <jaycjacob@gmail.com>
2022-06-09 18:16:54 +0300

    second commit

--- main.php
+++ main.php
@@ -1,42 +1,75 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=records",$username,$password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "CREATE DATABASE IF NOT EXISTS records";
-  $sql2 = "CREATE TABLE IF NOT EXISTS publishers (
-    publisher_id INT AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (publisher_id)
-)";
-$sql3 = 'SELECT publisher_id, name 
-FROM publishers';
-  $fname = $_POST["fname"];
-  $sql = "INSERT INTO `publishers` (publisher_id,name) VALUES ('0','$fname')";
-  // use exec because no results are returned.
-  //echo $conn->exec("show tables");
-  $conn->exec($sql);
-  $conn->exec($sql2);
-  $statement = $conn->query($sql3);
-  $publishers = $statement->fetchAll(PDO::FETCH_ASSOC);
-  print_r($publishers);
-  if ($publishers) {
-    foreach ($publishers as $publisher) {
-      echo $publisher['name'] . '<br>';
-      echo $publisher['publisher_id'];
-    }
-  }
- //echo $conn->query("show databases");
- //$conn->closeCursor();
-
-  //$conn->exec($sql);
-  echo "CONNECT SUCCESSFULLY.<br>";
-} catch (PDOException $e) {
-  echo "connection failed <br>". $e->getMessage();
-}
-$conn = null;
+include 'insertmain.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <style>
+      body{
+    background-color: rgba(255, 255, 255,0.2);
+}
+
+form {
+				width: 50%;
+				margin: 40px auto;
+			}
+input,
+label{
+		display: block;
+		width: 80%;
+		margin-left: 20px;
+		padding: 10px 0;
+}
+input[type="submit"] {
+				margin: 20px;
+				color: white;
+				background-color: dodgerblue;
+				font-weight: bold;
+				border-radius: 10px;
+			}
+.success {
+				color: green;
+				font-weight: bold;
+			}
+.error {
+				margin-left: 20px;
+				color: red;
+				font-weight: bold;
+			}
+    </style>
+  </head>
+  <body>
+   <h1>Enter Your details.</h1>
+   <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+    <h4>Employees Registration Form </h4>
+     <?php echo $formMsg ?>
+     <div>
+     <label for="phone">Phone Number1:</label><br>
+     <input type="number" id="pnumber" name="pnumber" class="input-text">
+     <span class="error"> <?php echo $pnumberError ?> </span>
+     </div>
+    <div>
+     <label for="fname">First Name:</label><br>
+     <input type="text" id="fname" name="fname" class="input-text">
+     <span class="error"> <?php echo $fnameError ?> </span>
+    </div>
+    <div>
+     <label for="lname">Second Name:</label><br>
+     <input type="text" id="lname" name="lname"class="input-text">
+     <span class="error"> <?php echo $lnameError ?> </span>
+    </div>
+    <div>
+     <label for="email">Email:</label><br>
+     <input type="email" id="email" name="email" class="input-text">
+     <span class="error"> <?php echo $emailError ?> </span>
+    </div>
+     <input type="submit" value="Submit"">
+
+   </form>
+
+  </body>
+</html>
